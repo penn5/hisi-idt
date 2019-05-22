0c3d03d6f38ff794d02256bd17740128b575a47b
Hackintosh <hackintosh5@wildfyre.net>
2019-05-22 12:04:50 +0100

    missing return

--- idtconfig.py
+++ idtconfig.py
@@ -15,9 +15,8 @@ def get_images(xml: bytes):
     ddr_images = tree.xpath("(//configurations/configuration)[1]/bootloaderimage_ddr/image")
     std_images = tree.xpath("(//configurations/configuration)[1]/bootloaderimage/image")
     images = {int(img.get("address"), 0):img.text for img in ddr_images}
-    print(images)
     ddr_ids = [img.get("identifier") for img in ddr_images]
     for img in std_images:
         if not img.get("identifier") in ddr_ids:
             images[int(img.get("address"), 0)] = img.text
-    print(images)
+    return images
