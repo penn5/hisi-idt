aef0954328807774f4b75828c1e6ad33e4fb8b4e
Hackintosh <hackintosh5@wildfyre.net>
2019-05-22 12:28:22 +0100

    Fix paths

--- main.py
+++ main.py
@@ -11,16 +11,16 @@
 import imageflasher
 import idtconfig
 
-import argparse
+import argparse, os
 
 def main(config, device, chip=""):
     # First parse the config
-    images = idtconfig.get_images(config)
+    images = idtconfig.get_images(config.read())
     flasher = imageflasher.ImageFlasher(chip)
     if device != False: # We have to check not False rather than just True, because None evaluates to False, and None should be passed intact
         flasher.connect_serial(device)
     for addr, fil in images.items():
-        flasher.download_from_disk(os.path.join(os.path.dirname(config.path), fil.replace("/", os.path.sep)), addr)
+        flasher.download_from_disk(os.path.join(os.path.dirname(config.name), fil.replace("/", os.path.sep)), addr)
     print("Flash successful!")
 
 if __name__ == "__main__":
@@ -33,6 +33,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.""")
     parser.add_argument("--norun", "-n", action="store_false", dest="run")
     parser.add_argument("--device", "-d")
-    parser.add_argument("config", type=argparse.FileType("rb"))
+    parser.add_argument("config")
     args = parser.parse_args()
-    main(args.config.read(), args.device if args.run else False)
+    with open(args.config, "rb") as f:
+        main(f, args.device if args.run else False)
