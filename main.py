10fb4b213696385bf8295bc1d02690aef4d4336e
Hackintosh <hackintosh5@wildfyre.net>
2019-05-22 11:58:34 +0100

    Rename idt config parser to prevent stdlib collisions

--- main.py
+++ main.py
@@ -9,13 +9,13 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import imageflasher
-import configparser
+import idtconfig
 
 import argparse
 
 def main(config, device, chip=""):
     # First parse the config
-    images = configparser.get_images(config)
+    images = idtconfig.get_images(config)
     flasher = imageflasher.ImageFlasher(chip)
     if device != False: # We have to check not False rather than just True, because None evaluates to False, and None should be passed intact
         flasher.connect_serial(device)
