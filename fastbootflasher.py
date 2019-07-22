0a87a08e6e91d665f3caaa28884ee44d4c491914
penn5 <penn5@users.noreply.github.com>
2019-07-22 22:33:51 +0100

    Catch ultraflash not permitted

--- fastbootflasher.py
+++ fastbootflasher.py
@@ -3,12 +3,15 @@
 from adb.fastboot import FastbootCommands, FastbootRemoteFailure, FastbootStateMismatch, FastbootInvalidResponse
 from adb.adb_commands import AdbCommands
 from adb.usb_exceptions import *
-import time
+import time, functools
 
-def info_cb(msg):
+def info_cb(fdev, partition, image, msg):
     if msg.header == b"FAIL":
         print(msg)
-        raise RuntimeException("Flash Failed")
+        if msg.message == b"ultraflash not permitted\x00":
+            # try normal flash
+            fdev.FlashFromFile(partition, file, info_cb=functools.partial(info_cb, fdev, partition, image), progress_callback=progress_cb)
+        raise RuntimeError("Flash Failed")
     print(msg)
 
 def progress_cb(current, total):
@@ -26,7 +29,7 @@ def flash(parts):
     for partition, file in parts:
         # File must be a filename, not a file()
         if partition == "huawei_crc_check":
-            fdev.FlashFromFile(partition, file, info_cb=info_cb, progress_callback=progress_cb)
+            fdev.FlashFromFile(partition, file, info_cb=functools.partial(info_cb, fdev, partition, file), progress_callback=progress_cb)
             continue
-        fdev._SimpleCommand(b'ultraflash', arg=partition, info_cb=info_cb, timeout_ms=0)
+        fdev._SimpleCommand(b'ultraflash', arg=partition, info_cb=functools.partial(info_cb, fdev, partition, file), timeout_ms=0)
         fdev.Download(file, info_cb=info_cb, progress_callback=progress_cb)
